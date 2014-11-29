b5c501a2bcf67163404058e7e6fbcc8eb17ba6de
joneshf <jones3.hardy@gmail.com>
2014-11-29 15:19:14 -0800

    Updated dependencies and added `x`, `y`, `z`.

--- README.md
+++ README.md
@@ -8,6 +8,12 @@
 
     _2 :: forall a b c. Lens (Tuple a b) (Tuple a c) b c
 
+    x :: forall b a r. Lens { x :: a | r } { x :: b | r } a b
+
+    y :: forall b a r. Lens { y :: a | r } { y :: b | r } a b
+
+    z :: forall b a r. Lens { z :: a | r } { z :: b | r } a b
+
 
 ## Module Optic.Refractor.Prism
 
--- src/Optic/Refractor/Lens.purs
+++ src/Optic/Refractor/Lens.purs
@@ -9,3 +9,12 @@ module Optic.Refractor.Lens where
 
   _2 :: forall a b c. Lens (Tuple a b) (Tuple a c) b c
   _2 a2fb (Tuple a b) = Tuple a <$> a2fb b
+
+  x :: forall b a r. Lens {x :: a | r} {x :: b | r} a b
+  x f o = (\x' -> o{x = x'}) <$> f o.x
+
+  y :: forall b a r. Lens {y :: a | r} {y :: b | r} a b
+  y f o = (\y' -> o{y = y'}) <$> f o.y
+
+  z :: forall b a r. Lens {z :: a | r} {z :: b | r} a b
+  z f o = (\z' -> o{z = z'}) <$> f o.z
